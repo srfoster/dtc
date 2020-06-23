ba569a0e1115ebc415aa1b5b52c66fca880c945f
Stephen Foster <stephen@thoughtstem.com>
2020-06-23 13:47:25 -0700

    adding image function to frames/cats lang, closes #8

--- frames/cats.rkt
+++ frames/cats.rkt
@@ -2,6 +2,7 @@
 
 (provide 
  (all-from-out "../story/cats.rkt")
+ (all-from-out "story-images/main.rkt")
  (rename-out 
   [normal-begin #%module-begin]))
 
@@ -9,4 +10,4 @@
   dtc/frames/cats)
 
 (require (except-in "../story/cats.rkt" #%module-begin))
-
+(require "story-images/main.rkt")
